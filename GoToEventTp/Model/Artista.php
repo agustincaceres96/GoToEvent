06839a728ccbf61f6183f7a7b323d77a735b563f
agustin <agustin.caceres96@hotmail.com>
2018-09-27 11:35:54 -0300

    clases

---
+++ GoToEventTp/Model/Artista.php
@@ -0,0 +1,33 @@
+<?php
+namespace Artista;
+
+class Artista{
+    private firstName;
+    private lastName;
+
+
+
+
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    public function getLastName(){
+        return $this->lastName;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
--- GoToEventTp/index.php
+++ GoToEventTp/index.php
@@ -1,37 +1,3 @@
-<?php
-    require_once("Autoload.php");
-    use Modelo\Usuario as Usuario;
-    
-    $noPost = false;
 
-    if($_POST){
-        $nombre = isset($_POST["nombre"]) ? $_POST["nombre"] : "Nombre no cargado";
-        $password = isset($_POST["password"]) ? $_POST["password"] : "Apellido no cargado";
-        $email = isset($_POST["email"]) ? $_POST["email"] : "Email no cargado";
-        
-        $defaultEmail= "admin";
-        $defaultPassword= "admin";
-
-        if(($email == $defaultEmail) && ($password == $defaultPassword))
-        {
-            
-            $user= new Usuario();
-            $user->setEmail($email);
-            $user->setName($nombre);
-            $user->setPassword($password);
-            
-//            session_start();
-//            
-          $_SESSION["userLogged"] = $user;
-            
-            header("location:Main.php");
-        }
-        else
-            echo "chau";
-     
- }
-    else{
-        echo "<h1>No hay post</h1>";
-        $noPost = true; //variable para esconder todo el html en caso de que no halla post
-    } 
-?>
+ <HTML> 
+ </HTML>
